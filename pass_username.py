f61b7f3e515c2250ba5601825d4a8c5c2ee303c5
tvstheesan-lgtm <tvstheesan@gmail.com>
2026-07-05 20:21:20 +0200

    while function pythoncode

---
+++ pass_username.py
@@ -0,0 +1,21 @@
+User_Name="Siva"
+Richtiges_Password="12345"
+
+versuche=0
+Enter_Name=input("Your User _Name please;")
+if Enter_Name == User_Name:
+     while versuche<3:
+        Passwort=input("Passwort eigeben;")
+        if Passwort==Richtiges_Password:
+            print("Welcome to Myworld!")
+            break
+
+        else:
+            versuche= versuche + 1
+            print("Wrong passwort!")
+
+     if versuche==3:
+        print("Contact admin ")
+
+else:
+    print("User is not found")
